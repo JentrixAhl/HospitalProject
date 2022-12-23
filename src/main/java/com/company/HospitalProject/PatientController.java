47537b7dff5f2748c67400fbb83a13def44583a4
JentrixAhl <jentyatrix@gmail.com>
2022-12-23 22:18:41 +0100

    Update PatientController.java

--- src/main/java/com/company/HospitalProject/PatientController.java
+++ src/main/java/com/company/HospitalProject/PatientController.java
@@ -3,14 +3,21 @@ package com.company.HospitalProject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class PatientController {
+	
+		@GetMapping("/")
+		@ResponseBody
+		public String HospitalPage() {
+			return "hospital.jsp";
+	}
 	
 		Patient patient = new Patient("Mary", "102030", 30);
 		
@@ -18,27 +25,32 @@ public class PatientController {
 		PatientList patientList;
 		
 		@GetMapping("/Mary")
+		@ResponseBody
 		public String getPatient() {
 			return patient.toString();
 		}
 		
 		@GetMapping("/patient")
+		@ResponseBody
 		public Patient getPatient(String personalNumber) {
 			return patientList.getPatientByPersonalNumber(personalNumber);
 		}
 		
 		@GetMapping("/patients")
+		@ResponseBody
 		public Patient[] getPatientArray() {
 			return patientList.getPatientsAsArray();
 		}
 		
 		@PostMapping("patient")
+		@ResponseBody
 		public String addPatient(Patient patient) {
 			patientList.add(patient);
 			return "Patient is admitted successfully!";
 		}
 		
 		@PutMapping("patient")
+		@ResponseBody
 		public ResponseEntity<String> changeName(String personalNumber, String newName) {
 			boolean success = patientList.changeName(personalNumber, newName);
 			if (success) {
@@ -48,6 +60,7 @@ public class PatientController {
 		}
 		
 		@DeleteMapping("patient")
+		@ResponseBody
 		public ResponseEntity<String> deletePatient(String personalNumber) {
 			boolean success = patientList.deletePatient(personalNumber);
 			if (success) {
